bd9a359c035df3fbc7d5368f60f200e0b7d4cb22
Kuznetsov Maxim <maximxlssoff@gmail.com>
2021-06-13 16:14:50 +0300

    refactoring and small changes really tiny

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ import os
 
 client = Client()
 
-CHANNEL_ID = 853587303100448778
-TOKEN = os.environ.get("DISCORD-TOKEN")
+# get enviroment variables and check if they are present
+CHANNEL_ID = os.environ.get("TEXT_CHANNEL_ID")
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
-textch = None
-
+if not CHANNEL_ID:
+    raise Exception("can't get text channel id from enviroment")
 if not TOKEN:
     raise Exception("can't get token from enviroment")
 
+# make CHANNEL_ID an integer
+CHANNEL_ID = int(CHANNEL_ID)
+
+# global TextChannel of configured text channel
+# gets set up in on_ready
+textch = None
+
+# sets up everything that needs auntefication
 @client.event
 async def on_ready():
     global textch
     textch = client.get_channel(CHANNEL_ID)
+    print("redy")
 
-
+# VOICE NOTIFY FUNCTIONALITY ------------------------------
+# sends a notify into configured text channel when user leaves, reconnects or joins voice channel
 @client.event
 async def on_voice_state_update(member, before, after):
-    global textch
     overwrites = textch.overwrites
     if before.channel != None and after.channel == None:
         try:
@@ -36,5 +46,6 @@ async def on_voice_state_update(member, before, after):
     elif before.channel != None and after.channel != None:
         await textch.send(f"**{member.display_name}** переместился из чата **{before.channel.name}** в чат **{after.channel.name}**")
     await textch.edit(overwrites=overwrites)
+# VOICE NOTIFY FUNCTIONALITY END --------------------------
 
 client.run(TOKEN)
